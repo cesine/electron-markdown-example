7d88aefe028f7e6658ec4b3a665ceec39e12587b
cesine <cesine@yahoo.com>
2016-03-24 13:24:41 -0400

    nwjs version

--- finished-demo/main.js
+++ finished-demo/main.js
@@ -1,32 +1,16 @@
 'use strict';
 
-var electron = require('electron');
-var app = electron.app;  // Module to control application life.
-var BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+var gui = require('nw.gui');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
-
-
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
+var mainWindow = gui.Window.get();
+// Extend application menu for Mac OS
+if (process.platform == "darwin") {
+  var menu = new gui.Menu({
+    type: "menubar"
   });
-});
+  menu.createMacBuiltin && menu.createMacBuiltin(window.document.title);
+  mainWindow.menu = menu;
+}
+
+mainWindow.show();
+mainWindow.showDevTools();
